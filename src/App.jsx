66641d8b249fdd00b4f002f1a611c5f43dd20efb
Youssef Hany <youssefhany.2005.yh@gmail.com>
2025-05-28 02:14:45 +0300

    Update Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
-import Home from './pages/Home.jsx'
+import Home from './pages/home/Home.jsx'
 import './App.css'
 function App() {
   return (
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 // import React, { Suspense } from 'react';
-import Header from '../components/Header/Header.jsx'
-import OurServices from '../components/OurServices/OurServices.jsx'
-import Skills from '../components/Skills/Skills.jsx'
-import Projects from '../components/Projects/Projects.jsx'
-import Contact from '../components/Contact/Contact.jsx'
+import Header from '../../components/Header/Header.jsx'
+import OurServices from '../../components/OurServices/OurServices.jsx'
+import Skills from '../../components/Skills/Skills.jsx'
+import Projects from '../../components/Projects/Projects.jsx'
+import Contact from '../../components/Contact/Contact.jsx'
 import './home.css'
 
 // Background Matrix Rain
