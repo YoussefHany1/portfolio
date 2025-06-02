0c05d332eba26e47037d957c6c2fca67cf260e4b
YoussefHany1 <youssefhany.2005.yh@gmail.com>
2025-06-02 18:55:54 +0300

    Add react-router-dom and implement routing; create Projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Home from './pages/home/Home.jsx'
+import Projects from './pages/projects/Projects.jsx'
 import './App.css'
+
 function App() {
+  const [loading, setLoading] = useState(true);
+useEffect(() => {
+  const handlePageLoad = () => {
+    setLoading(false);
+  };
+
+  if (document.readyState === 'complete') {
+    setLoading(false);
+  } else {
+    window.addEventListener('load', handlePageLoad);
+  }
+
+  return () => window.removeEventListener('load', handlePageLoad);
+}, []);
+
+if (loading) {
+  return (
+    <div className="loader d-flex justify-content-center align-items-center vh-100">
+      <div data-glitch="Loading..." className="glitch">Loading...</div>
+    </div>
+  );
+}
   return (
     <>
-      <Navbar />
-        <Home />
+    <Navbar />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
       <Footer />
     </>
   )
---
+++ src/pages/projects/Projects.jsx
@@ -0,0 +1,11 @@
+// import styles from './Projects.module.css'
+
+function Projects() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+export default Projects
